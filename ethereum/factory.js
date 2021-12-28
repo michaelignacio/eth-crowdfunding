1487cdbfce3c64f43111ff9b150dab04ee8a8bdd
Michael <mikelignacio@gmail.com>
2021-12-28 19:38:04 +0800

    Add button and card group

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  0x9eec7d973660a2046577107DaB0E9D8d5BC4621E
+  '0x9eec7d973660a2046577107DaB0E9D8d5BC4621E'
 );
 
 export default instance;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Card, Button } from 'semantic-ui-react';
+import factory from '../ethereum/factory';
 
-export default () => {
-    return (
-        <h1>Campaign list page</h1>
-    );
-};
+class CampaignIndex extends Component {
+  static async getInitialProps() {
+    const campaigns = await factory.methods.getDeployedCampaigns().call();
+    return { campaigns };
+  }
+
+  renderCampaigns() {
+    const items = this.props.campaigns.map(address => {
+      return {
+        header: address, 
+        description: <a>View Campaign</a>,
+        fluid: true
+      };
+    });
+
+    return <Card.Group items={items} />;
+  }
+
+  render() {
+    return <div>
+      <link
+        async
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css"
+      />
+      <script src="https://cdn.jsdelivr.net/npm/semantic-ui-react/dist/umd/semantic-ui-react.min.js"></script>
+      <h3>Open Campaigns</h3>
+      {this.renderCampaigns()}
+      <Button
+        content="Create Campaign"
+        icon="add circle"
+        primary
+      />
+      </div>
+  }
+}
+
+export default CampaignIndex;
