814edf6646df9c3f61bafba81bf9e866a929cae1
Michael <mikelignacio@gmail.com>
2022-01-03 10:24:34 +0800

    Fix markup issue

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -1,9 +1,9 @@
-import web3 from "./web3";
-import CampaignFactory from "./build/CampaignFactory.json";
+import web3 from './web3';
+import CampaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  "0x5152215b9fde6535AFaC7a7f3b8eaF783286b975"
+  '0xf2Af215663E8335d6D606b8D1298765118060638'
 );
 
 export default instance;
