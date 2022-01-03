aa94da03c1b670941d687753a2148569f83865c5
Michael <mikelignacio@gmail.com>
2022-01-03 10:33:40 +0800

    Add more cards

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Card } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
 import Campaign from '../../ethereum/campaign';
+import web3 from '../../ethereum/web3';
 
 class CampaignShow extends Component {
   static async getInitialProps(props) {
@@ -34,6 +35,30 @@ class CampaignShow extends Component {
         description:
           'The manager created this campaign and can create requests to widthdraw money',
         style: { overflowWrap: 'break-word' },
+      },
+      {
+        header: minimumContribution,
+        meta: 'Minimum Contribution (wei)',
+        description:
+          'You must contribute at least this much wei to become an approver',
+      },
+      {
+        header: requestsCount,
+        meta: 'Number of requests',
+        description:
+          'A request tries to withdraw money from the contract. Requests must be approved by approvers.',
+      },
+      {
+        header: approversCount,
+        meta: 'Number of approvers',
+        description:
+          'Number of people who have already donated to this campaign',
+      },
+      {
+        header: web3.utils.fromWei(balance, 'ether'),
+        meta: 'Campaign Balance (ether)',
+        description:
+          'The balance is how much mone this campaign has left to spend.',
       },
     ];
 
