92a56aa938887d14eb9c6da392f04a4b287e0964
Michael <mikelignacio@gmail.com>
2022-01-03 09:57:40 +0800

    Re-compile and deploy Campaigns contract

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -1,9 +1,9 @@
-import web3 from './web3';
-import CampaignFactory from './build/CampaignFactory.json';
+import web3 from "./web3";
+import CampaignFactory from "./build/CampaignFactory.json";
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  '0x9eec7d973660a2046577107DaB0E9D8d5BC4621E'
+  "0x5152215b9fde6535AFaC7a7f3b8eaF783286b975"
 );
 
 export default instance;
