3a6265f8e1cf7cbe4460e521780e5b2cab0959a5
Michael <mikelignacio@gmail.com>
2022-01-03 09:26:40 +0800

    Add routes, router

--- components/Layout.js
+++ components/Layout.js
@@ -12,7 +12,7 @@ const Layout = (props) => {
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css"
         />
-        <script src="https://cdn.jsdelivr.net/npm/semantic-ui-react/dist/umd/semantic-ui-react.min.js"></script>
+        {/* <script src="https://cdn.jsdelivr.net/npm/semantic-ui-react/dist/umd/semantic-ui-react.min.js"></script> */}
       </Head>
       <Header />
       {props.children}
