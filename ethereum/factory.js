f963c346409e605f463efe5bcbcada0b5bf0d1ff
Michael <mikelignacio@gmail.com>
2022-01-07 00:38:17 +0800

    Rebuild src

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  '0xcF987Bf7FAcdc11d3BECa2f3b830C65F02FE62b0'
+  '0x67A2BA390eAA778e8b4FA5A8A0c66E3Cacd6b180'
 );
 
 export default instance;
